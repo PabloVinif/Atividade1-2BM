deacc42ccdba419ce3f034804b21236c036a6010
PabloVinif <163577161+PabloVinif@users.noreply.github.com>
2024-10-15 15:56:02 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Aplicação Bancária',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
       ),
       home: TelaListaContas(), // Tela inicial.
     );
